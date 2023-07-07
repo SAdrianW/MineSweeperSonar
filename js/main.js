9931db9d05aab807ec2a17f7452c07487ca2c4b5
SAdrianW <wellman.shaun@gmail.com>
2023-07-07 19:24:36 +1000

    initial CSS for testing. More pseudocode and JS tests for basic functions

--- js/main.js
+++ js/main.js
@@ -2,83 +2,108 @@
 const DIFFICULTY_LVL = {
     easy: {mines: 10, boardSize: 'small'},
     med: {mines: 40, boardSize: 'medium'},
-    hard: {mines: 10, boardSize: 'large'}
+    hard: {mines: 99, boardSize: 'large'},
+    square: {mines: 10, boardSize: 'square'},
+    test: {mines: 3, boardSize: 'test'}
 };
-// Will always reference at least easy. With diff
+// Will always reference at least square. With diff
 
 const BOARD_OPTIONS = {
     easy: {rows: 9, columns: 19},
     med: {rows: 15, columns: 31},
-    hard: {rows: 21, columns: 43}
+    hard: {rows: 21, columns: 43},
+    square: {rows: 10, columns: 10},
+    test: {rows: 4, columns: 4}
 };
 
 
 /*------ State Variables ------*/
-let board;  // array, square (or cone or cone if enough time)
+let board;      // array, square (or cone if enough time)
 
-let timer;  // counts time taken from first click. timer element that always starts at 0, counts up as time passes while result = inGame
+let timer;      // counts time taken from first click. timer element that always starts at 0, counts up as time passes while result = inGame
 
-let result; // initially at inGame, changes to gameWon or gameLost
-// ?O?  let difficulty; // ? Optional ? easy, med or hard
-let visibility;
-let minesRemaing; // depends on difficulty. easy = 10, med = 40, hard = 99
+let result;         // initially at inGame, changes to gameWon or gameLost
+
+let difficulty;     // ? Optional ? easy, med or hard
+
+let visibility;     // cells change to visable after being clicked
+
+let minesRemaing;   // depends on difficulty. easy = 10, med = 40, hard = 99
  
-
+let mineLocations;  // num = ^. random location (use Math.random ?). But how to assign them if cell divs aren't named. TODO: figure that out.
 
 /*------ Cached Elements ------*/
-const resetBtn = document.getElementById('reset');
+// Elements to be accessed repeatedly. saved here for efficency
 
+const resetBtn = document.getElementById('reset');
+const diffSelection = document.getElementById(diffChoice);
 
 /*------ Event Listeners ------*/
-document.getElementById('cell').addEventListener('click', sonarPing); // player choice of cell selection
-document.getElementById('diff-btn').addEventListener('click', diffChoice)
+// document.querySelector('board').addEventListener('click', sonarPing); // player choice of cell selection
+// document.getElementById('diff-options').addEventListener('click', diffChoice);
+// document.getElementById('reset').addEventListener('click', initialise);
 
 
 /*------ Functions ------*/
+
 initialise();
 // prepares the game to be played
 
 
 function initialise() {
-    board =
-    timer = 0;
-    // ?O?  difficulty = 
-    result = inGame;
-    visibility = hidden; // cells change to visable after bing clicked
-    minesRemaing = diff.mines
-
-    render();
+    board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ];
+     // path for desired board info: BOARD_OPTIONS[DIFFICULTY_LVL.boardSize] TODO: find function that makes array based on paramaters supplied
+    // timer = 0;
+    // result = inGame;
+    // visibility = hidden; 
+    // difficulty = diffChoice;
+    // minesRemaing = DIFFICULTY_LVL.mines;
+    // render();
 };
 
 
 
 
-function render() {
-    renderBoard();
-    renderTimer();
-    renderMines();
-};
+// function render() {
+//     renderBoard();
+//     // renderTimer();
+//     // renderMines();
+// };
 // Render: displays the game to the DOM
 
-function renderBoard() {
+// function renderBoard() {
+//     board.forEach((colArr, colIdx) => {
+//         colArr.forEach((cellVal, rowIdx) => {
+//             const cellId = `r${rowIdx}c${colIdx}`;
+//             const cellEl = document.getElementById(cellId);
+//             cellEl.style.backgroundColor = "blue";
+//             cellEl.innerText = "";
+//         })
+//     });
+// };
 
-};
+// function renderTimer() {
 
-function renderTimer() {
+// };
 
-};
+// function renderMines() {
 
-function renderMines() {
-
-};
+// };
 
 
+// guard: if (evt.target.tagName !== 'CELL') return; // this should prevent clicks from happening "out of bounds"
+                      // ^ or className, if that works
 
-function sonarPing() {
-    // guard: if (evt.target.tagName !== 'CELL') return; // this should prevent clicks from happening "out of bounds"
-                          // ^ or className, if that works
+function sonarPing(evt) {
+    console.log(evt.target)
     render();
-    EventTarget.class = visable
+
+    // EventTarget.class = visable;
 };
 // sonarPing: responds to user interaction. (probably not the best name, but it is on theme)
 
@@ -89,5 +114,7 @@ function sonarPing() {
 
 // ?O?  diffuculty. defaults to easy if nothing is picked
 function diffChoice() {
+    // difficulty = square if no 'click' detected. If click detected then use that result.
+    return difficulty = 'test';
 
 };

39616831168d626b6baa1ef7eb38eff37a835410
SAdrianW <wellman.shaun@gmail.com>
2023-07-10 11:26:44 +1000

    added very basic scripted functionality

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 /*------ Constants ------*/
 const DIFFICULTY_LVL = {
-    easy: {mines: 10, boardSize: 'small'},
-    med: {mines: 40, boardSize: 'medium'},
-    hard: {mines: 99, boardSize: 'large'},
+    // easy: {mines: 10, boardSize: 'small'},
+    // med: {mines: 40, boardSize: 'medium'},
+    // hard: {mines: 99, boardSize: 'large'},
     square: {mines: 10, boardSize: 'square'},
     test: {mines: 3, boardSize: 'test'}
 };
 // Will always reference at least square. With diff
 
 const BOARD_OPTIONS = {
-    easy: {rows: 9, columns: 19},
-    med: {rows: 15, columns: 31},
-    hard: {rows: 21, columns: 43},
+    // easy: {rows: 9, columns: 19},
+    // med: {rows: 15, columns: 31},
+    // hard: {rows: 21, columns: 43},
     square: {rows: 10, columns: 10},
     test: {rows: 4, columns: 4}
 };
 
 
 /*------ State Variables ------*/
-let board;      // array, square (or cone if enough time)
+let board;      // array; square (or cone if enough time)
 
 let timer;      // counts time taken from first click. timer element that always starts at 0, counts up as time passes while result = inGame
 
@@ -30,7 +30,7 @@ let visibility;     // cells change to visable after being clicked
 
 let minesRemaing;   // depends on difficulty. easy = 10, med = 40, hard = 99
  
-let mineLocations;  // num = ^. random location (use Math.random ?). But how to assign them if cell divs aren't named. TODO: figure that out.
+let mineLocations;  // num = ^. randomCell (use Math.random row * collumn). While loop to iterate loops = ^
 
 /*------ Cached Elements ------*/
 // Elements to be accessed repeatedly. saved here for efficency
@@ -39,7 +39,7 @@ const resetBtn = document.getElementById('reset');
 const diffSelection = document.getElementById(diffChoice);
 
 /*------ Event Listeners ------*/
-// document.querySelector('board').addEventListener('click', sonarPing); // player choice of cell selection
+document.querySelector('.board').addEventListener('click', sonarPing); // player choice of cell selection
 // document.getElementById('diff-options').addEventListener('click', diffChoice);
 // document.getElementById('reset').addEventListener('click', initialise);
 
@@ -47,45 +47,47 @@ const diffSelection = document.getElementById(diffChoice);
 /*------ Functions ------*/
 
 initialise();
-// prepares the game to be played
-
+// prepares the game to be played, readies all state
 
 function initialise() {
+    // rotate 90deg counter-clockwise to visualise to DOM.
     board = [
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
     ];
-     // path for desired board info: BOARD_OPTIONS[DIFFICULTY_LVL.boardSize] TODO: find function that makes array based on paramaters supplied
-    // timer = 0;
+    // // path for desired board info: BOARD_OPTIONS[DIFFICULTY_LVL.boardSize] TODO: find function that makes array based on paramaters supplied
+    timer = 0;
     // result = inGame;
     // visibility = hidden; 
-    // difficulty = diffChoice;
-    // minesRemaing = DIFFICULTY_LVL.mines;
-    // render();
+    difficulty = diffChoice;
+    // minesRemaing = DIFFICULTY_LVL.{diffChoice.mines};
+    render();
 };
 
 
 
 
-// function render() {
-//     renderBoard();
-//     // renderTimer();
-//     // renderMines();
-// };
-// Render: displays the game to the DOM
+// // Render: displays/ visualise the game to the DOM
+function render() {
+    renderBoard();
+    // renderTimer();
+    // renderMines();
+};
 
-// function renderBoard() {
-//     board.forEach((colArr, colIdx) => {
-//         colArr.forEach((cellVal, rowIdx) => {
-//             const cellId = `r${rowIdx}c${colIdx}`;
-//             const cellEl = document.getElementById(cellId);
-//             cellEl.style.backgroundColor = "blue";
-//             cellEl.innerText = "";
-//         })
-//     });
-// };
+function renderBoard() {
+    board.forEach(function(colArr, colIdx) {
+        colArr.forEach(function(cellVal, rowIdx) {
+            const cellId = `r${rowIdx}c${colIdx}`;
+            const cellEl = document.querySelector(`.${cellId}`);
+            // console.log(cellEl);
+            // cellEl.classList.add("hidden"); todo delete?
+            // cellEl.style.backgroundColor = 'purple';
+            // cellEl.innerText = "";
+        });
+    });
+};
 
 // function renderTimer() {
 
@@ -100,7 +102,11 @@ function initialise() {
                       // ^ or className, if that works
 
 function sonarPing(evt) {
-    console.log(evt.target)
+    console.log(evt.target, 'ping');
+    evt.target.classList.remove('hidden');
+    evt.target.classList.add('revealed');
+    console.log(evt.target, 'woop');
+    
     render();
 
     // EventTarget.class = visable;
@@ -118,3 +124,7 @@ function diffChoice() {
     return difficulty = 'test';
 
 };
+
+
+// reset: may not add hiddem class. or may add second. currently hard coded html
+// might need to move hidden to init function.
---
+++ js/second.js
@@ -0,0 +1,9 @@
+// min viable prod (MVP) = 10*10 playable game
+// testing reqs: 
+//      4*4 board
+//         starting hidden for all cells
+//          
+//      basic interactivity:
+//          cells change when clicked
+
+
